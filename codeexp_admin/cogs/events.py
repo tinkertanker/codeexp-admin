4a94393daf2b96033338f5491423fd5650d0bda1
Robert Xiu <xiurobert@gmail.com>
2022-06-05 08:56:46 +0800

    revert: remove welcome join

--- codeexp_admin/cogs/events.py
+++ codeexp_admin/cogs/events.py
@@ -85,16 +85,16 @@ class EventHandlers(commands.Cog):
         if member.guild.id != self.bot.debug_guilds[0]:
             return
         dm = await member.create_dm()
-        captcha_text = create_captcha(self.bot.sqlite_engine, member.id)
-        await dm.send(f"Welcome to the CODE_EXP 2022 server\n"
-                      f"1. To get started, please react with a :white_check_mark: in the #rules channel "
-                      f"to see the other channels."
-                      f"2. Head to #botspam and type in `/usermod [category: YourCategory] "
-                      f"[group_num: YourGroupNumber]`. "
-                      f"Refer to the invite email for `YourCategory` and `YourGroupNumber`."
-                      f"3. Make sure you see the channels called `#cat-X-grp-Y-txt` and `cat-X-grp-Y-vc`. "
-                      f"These are your text and voice channels, private to you, your team, and your mentors. "
-                      f"4. If you have any questions or issues, please ask in #discord-help.")
+        # captcha_text = create_captcha(self.bot.sqlite_engine, member.id)
+        # await dm.send(f"Welcome to the CODE_EXP 2022 server\n"
+        #               f"1. To get started, please react with a :white_check_mark: in the #rules channel "
+        #               f"to see the other channels."
+        #               f"2. Head to #botspam and type in `/usermod [category: YourCategory] "
+        #               f"[group_num: YourGroupNumber]`. "
+        #               f"Refer to the invite email for `YourCategory` and `YourGroupNumber`."
+        #               f"3. Make sure you see the channels called `#cat-X-grp-Y-txt` and `cat-X-grp-Y-vc`. "
+        #               f"These are your text and voice channels, private to you, your team, and your mentors. "
+        #               f"4. If you have any questions or issues, please ask in #discord-help.")
         # await dm.send(f"Please reply to me with the following : `!captcha {captcha_text}`")
         # if member.name.lower().startswith("dsta mentor"):
         #     await member.add_roles(member.guild.get_role(self.bot.cfg.mentor_role))
